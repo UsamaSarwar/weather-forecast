35cc4c53d19a07a04eec211802385e1e67e33cb1
UsamaSarwar <UsamaSarwarOfficial@gmail.com>
2021-01-16 18:44:14 +0500

    Version 1.0.0

--- weather/app/includes/info.php
+++ weather/app/includes/info.php
@@ -1,6 +1,6 @@
 <?php
 
 define('SOFTWARE_NAME', 'Weather Forecast');
-define('SOFTWARE_AUTHOR', '');
-define('SOFTWARE_URL', '#');
+define('SOFTWARE_AUTHOR', 'Usama Sarwar');
+define('SOFTWARE_URL', 'https://usamasarwar.github.io');
 define('SOFTWARE_VERSION', '1.0.0');
